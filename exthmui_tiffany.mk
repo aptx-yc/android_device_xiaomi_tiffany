734195c0be398534322ba495ca9818c7d8fd783a
aptx-yc <78417944+aptx-yc@users.noreply.github.com>
2021-02-15 17:16:49 +0800

    Update exthmui_tiffany.mk

--- exthmui_tiffany.mk
+++ exthmui_tiffany.mk
@@ -1,15 +1,45 @@
-# Include LineageOS stuff
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-$(call inherit-product, devices/xiaomi/tiffany/lineage_tiffany.mk)
-$
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Include exthmui stuff
-$(call inherit-product, vendor/exthmui/device-exthmui.mk)
-$(call inherit-product, vendor/exthmui/BoardConfigVendor.mk)
+# Inherit from tiffany device
+$(call inherit-product, device/xiaomi/tiffany/device.mk)
 
-#WITH_DEXPREOPT := false
-PRODUCT_NAME := exthmui_tiffany
-PRODUCT-MODEL := Exthmui on the MI 5X
+# Inherit some common PixelExperience stuff.
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-EXTHMUI_VERSION := 10.0-UNOFFICAL
-EXTHMUI_BUILD_VERSION := $(EXTHMUI_VERSION)-$(shell date -u +%Y%m%d)
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := tiffany
+PRODUCT_NAME := aosp_tiffany
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 5X
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="tiffany-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "xiaomi/tiffany/tiffany:7.1.2/N2G47H/V9.2.2.0.NDBCNEK:user/release-keys"
